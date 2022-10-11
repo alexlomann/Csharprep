eb06feee31cb6be9a735f6620929f60c95b2080f
Alex Lohmann <8289222@gmail.com>
2022-10-11 20:17:44 +0300

    Семинар 44

---
+++ Sem_4.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Input number: ");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Sum numbers:  = {GetSum(A)}");
+
+int GetSum (int limit){
+    int sum = 0;
+    for (int i = 1; i <= limit; i++){
+        sum = sum + i;
+
+    }
+      return sum;  
+}
